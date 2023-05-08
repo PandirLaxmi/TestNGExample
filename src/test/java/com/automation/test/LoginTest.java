813437cd4118c6b524abb43523f271f0bbb2cf21
Laxmi <ranalxmi310@gmail.com>
2023-05-08 09:40:08 +0530

    first commit

---
+++ src/test/java/com/automation/test/LoginTest.java
@@ -0,0 +1,10 @@
+package com.automation.test;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+    @Test
+    public void verifyLoginSuccessful(){
+        System.out.println("test  - verify login Successful");
+}
+}
